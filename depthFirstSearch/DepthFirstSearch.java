6f4c65f1ed6585fdf89f728389bb7b82a2b57ec8
Pushp Raj <pushp277@gmail.com>
2025-03-23 14:23:59 +0900

    depth first search with Iteration

--- depthFirstSearch/DepthFirstSearch.java
+++ depthFirstSearch/DepthFirstSearch.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
-class DepthFirstSearch{
+
+class DepthFirstSearch implements DepthFirstSearchInterface{
     //We will first initialze adjacency List
     ArrayList<ArrayList<Integer>> adjList;
     ArrayList<Boolean> visited;
+    int N;
 
 DepthFirstSearch(Integer N){
+    this.N = N;
     adjList = new ArrayList<>();
     visited = new ArrayList<>();
     for(int i=0;i<N;i++){
@@ -14,37 +18,71 @@ DepthFirstSearch(Integer N){
         visited.add(false);
     }
 }
-public void depthFirstSearch(int u, ArrayList<Boolean> visited){ //Using Recursion
+
+public void DepthFirstSearchRec(int u, ArrayList<Boolean> visited){ //Using Recursion
     if(visited.get(u))
         return;
 
+        System.out.println(u);
     visited.set(u,true);
     for(int i=0;i<adjList.get(u).size();i++){
-        depthFirstSearch(adjList.get(u).get(i), visited);
+        DepthFirstSearchRec(adjList.get(u).get(i), visited);
     }
-    System.out.println(u);
+}
+
+public void DFS(int u){
+    Stack<Integer> stk = new Stack<>();
+    ArrayList<Boolean> visit = new ArrayList<>();
+
+    for(int i=0;i<N;i++){
+        visit.add(false);
+    }
+
+    stk.push(u);
+    visit.set(u,true);
+    ArrayList<Integer> arr = new ArrayList<>();
+    while(!stk.isEmpty()){
+        int top = stk.pop();
+        arr.add(top);
+        for(int i=0;i<adjList.get(top).size();i++){
+            if(!visit.get(adjList.get(top).get(i))){
+                stk.push(adjList.get(top).get(i));
+                visit.set(adjList.get(top).get(i),true);
+            }
+        }
+    }
+
+    for(int x: arr){
+        System.out.println(x);
+    }
 }
 
 public static void main(String[] args){
-    //Provide EdgeList Input
-    //The Input will be like [[x1,y1],[x2,y2],[x3,y3]]
-    Scanner input = new Scanner(System.in); 
-   
-    int N = input.nextInt();
-    input.nextLine();
-    String arrString = input.nextLine().replaceAll("[\\[\\]]", "");
-     DepthFirstSearch dfs = new DepthFirstSearch(N);
-    String[] splitArr = arrString.split(",");
+    try (//Provide EdgeList Input
+        //The Input will be like [[x1,y1],[x2,y2],[x3,y3]]
+    Scanner input = new Scanner(System.in)) {
+        int N = input.nextInt();
+        input.nextLine();
+        String arrString = input.nextLine().replaceAll("[\\[\\]]", "");
+         DepthFirstSearch dfs = new DepthFirstSearch(N);
+        String[] splitArr = arrString.split(",");
 
-    for(int i=0;i<splitArr.length-1 ;i+=2){
-        int a = Integer.parseInt(splitArr[i]);
-        int b = Integer.parseInt(splitArr[i+1]);
+        for(int i=0;i<splitArr.length-1 ;i+=2){
+            int a = Integer.parseInt(splitArr[i]);
+            int b = Integer.parseInt(splitArr[i+1]);
 
-        dfs.adjList.get(a).add(b);
-        dfs.adjList.get(b).add(a);
+            dfs.adjList.get(a).add(b);
+            dfs.adjList.get(b).add(a);
+        }
+
+        System.out.println("DFS Using Recussion");
+
+        dfs.DepthFirstSearchRec(0, dfs.visited);
+
+        System.out.println("DFS using Stack");
+        dfs.DFS(0);
+    } catch (NumberFormatException e) {
+        e.printStackTrace();
     }
-
-    
-    dfs.depthFirstSearch(0, dfs.visited);
 }
 };
---
+++ depthFirstSearch/DepthFirstSearchInterface.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public interface DepthFirstSearchInterface {
+    void DepthFirstSearchRec(int u, ArrayList<Boolean> visited);
+    void DFS(int u);
+}
